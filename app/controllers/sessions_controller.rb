dabc2a904a8a7e5cefb240526a047599a86ce582
Zac Willmington <zac.willmington@gmail.com>
2018-07-23 21:44:08 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,19 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-      binding.pry
-
       if params.include?(:name) && params[:name] != ''
-          binding.pry
             session[:name] = params[:name]
             redirect_to '/'
       else
-          binding.pry
             redirect_to '/sessions/new'
       end
   end
 
   def destroy
-
+      binding.pry
   end
 end
